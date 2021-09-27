0bb17767b94d4cb08952cef114314a36a0e5d5ec
Nathanaël Houn <contact@nathanaelhoun.fr>
2021-09-27 19:41:49 +0200

    Add proxy setup for dev

---
+++ src/setupProxy.js
@@ -0,0 +1,14 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: "https://sedna.univ-fcomte.fr",
+      pathRewrite: {
+        "^/api/v1": "/jsp/custom/ufc",
+      },
+      changeOrigin: true,
+    })
+  );
+};
