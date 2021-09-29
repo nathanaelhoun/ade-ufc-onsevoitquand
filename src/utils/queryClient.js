c1eff6d286ce89aebf508290bfac0547d3c8c4e6
Nathanaël Houn <contact@nathanaelhoun.fr>
2021-09-29 20:22:12 +0200

    Bigger cache

--- src/utils/queryClient.js
+++ src/utils/queryClient.js
@@ -4,7 +4,8 @@ const queryClient = new QueryClient();
 
 queryClient.setDefaultOptions({
   queries: {
-    cacheTime: 1000 * 60 * 10, // Keep informations in cache for ten minutes
+    cacheTime: 1000 * 60 * 60, // Keep informations in cache for 1 hour
+    staleTime: 1000 * 60 * 30, // Only refetch after 30 minutes
   },
 });
 
