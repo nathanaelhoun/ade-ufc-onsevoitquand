02ae8cf49cdddfa9ac430f731a1e5e6a91634a5c
Nathanaël Houn <contact@nathanaelhoun.fr>
2021-12-18 22:07:34 +0100

    feat(ui): Added hint about hovers

--- src/components/miscellaneous/Title.jsx
+++ src/components/miscellaneous/Title.jsx
@@ -19,9 +19,19 @@ const Title = () => (
 			Trouver un créneau pour se voir entre étudiants de l'
 			<abbr title="Université de Franche-Comté">UFC</abbr>.{" "}
 			<a href="https://github.com/nathanaelhoun/ade-ufc-onsevoitquand/">
-				<code style={{ display: "inline" }}>Code source</code>
+				<code>Code source</code>
 			</a>{" "}
 			par <a href="https://nathanaelhoun.fr/">@nathanaelhoun</a>.{" "}
+		</p>
+
+		<p className="help">
+			Conseil : 
+			<span className="mobile">
+				un appui long sur les noms de groupe raccourcis fait apparaître le nom entier.
+			</span>
+			<span className="desktop">
+				au survol des noms de groupes raccourcis, les noms entiers apparaissent.
+			</span>
 		</p>
 	</div>
 );
