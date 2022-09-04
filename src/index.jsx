77b3d14e1d4579059bcc1759879292b454391129
Nathanaël Houn <contact@nathanaelhoun.fr>
2022-09-04 22:56:02 +0200

    chore: upgrade to React18 and other deps

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { React } from "react";
+import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById("root")
-);
+const container = document.getElementById("app");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+
+root.render(<App />);
